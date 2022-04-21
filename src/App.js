ee49e8d9e02300cf75c247aeb9bbc3710e6c6c2b
brittany klose <brittany.klose@yahoo.com>
2022-04-21 14:08:32 -0400

    import just container now vs all files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
 import "./App.css";
 //import all files going to use 
-import Header from "./components/Header.js";
-import Footer from "./components/Footer";
-import Portfolio from "./Pages/portfolio";
+// import Header from "./components/Header.js";
+// import Footer from "./components/Footer";
+// import Portfolio from "./Pages/portfolio";
+import PortContainer from './components/PortContainer';
 
 function App () {
     return(
         <div className="app">
-        <Header />
-        <Portfolio/>
-        <Footer />
+        <PortContainer />
         </div>
     );
 };
