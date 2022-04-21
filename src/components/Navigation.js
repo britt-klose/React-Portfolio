909d1e0e458c9246fb2f85d8a9fd28c06e80b158
brittany klose <brittany.klose@yahoo.com>
2022-04-21 14:10:48 -0400

    nav tabs for header

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,57 @@
-import React from "react";
+import React from 'react';
 
+// TODO: Add a comment explaining how we are able to extract the key value pairs from props
+// Here we destructure our props into their own distinct variables
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
+          //
+          //checks is current pg home? if so make the nav link active. if not, nav link is not active
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          //  TODO: Add a comment explaining what this logic is doing
+          //is the current pg the ABOUT pg/sect? if so make nav link active, if not link is not active
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+          //  TODO: Add a comment explaining what this logic is doing
 
-function Navigation (){
-    return(
-        <div>
-        <p>Nav test</p>
-        </div>
-    )
-};
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Blog
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          //  TODO: Add a comment explaining what this logic is doing
 
-export default Navigation;
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
