1cca88a571b2739388981351be7280f41274f71b
brittany klose <brittany.klose@yahoo.com>
2022-04-26 13:09:33 -0400

    done except css file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
 //import all files going to use 
-// import Header from "./components/Header.js";
-// import Footer from "./components/Footer";
+ import Header from "./components/Header.js";
+ import Footer from "./components/Footer";
 // import Portfolio from "./Pages/portfolio";
 import PortContainer from './components/PortContainer';
 
 function App () {
     return(
         <div className="app">
+        <Header/>
         <PortContainer />
+        <Footer/>
         </div>
     );
 };
